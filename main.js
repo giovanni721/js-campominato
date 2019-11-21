a4a7bf5415472c7334813fb08a0a3f6c4f1ec8c9
giovanni <giovanni.giogio98@gmail.com>
2019-11-21 18:33:23 +0100

    fine prima parte

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var fiorinum = [];
 var bombenum = [];
+var campo = []
 
 // var usernum = prompt('Inserisci un numero da 1 a 100')
 function bomba() {
@@ -7,22 +8,25 @@ function bomba() {
     return numeribomba;
 }
 
+while (campo.length < 100) {
+    while (bombenum.length < 16) {
+        var continuo = bomba()
+        if(!bombenum.includes(continuo)) {
+            bombenum.push(continuo);
+        }
+    }
 
-while (bombenum.length < 16) {
-    var continuo = bomba()
-    if(!bombenum.includes(continuo)) {
-        bombenum.push(continuo);
+    while (fiorinum.length < 84) {
+        var fiori = bomba()
+        if (!fiorinum.includes(fiori)) {
+            fiorinum.push(fiori);
+        }
+    }
+    if (!bombenum.includes(fiorinum)) {
+        var campo = fiorinum.concat(bombenum);
     }
 }
 
-while (fiorinum.length < 84) {
-    var fiori = bomba()
-    if (!fiorinum.includes(fiori)) {
-        fiorinum.push(fiori);
-    }
-}
-
-var campo = fiorinum.concat(bombenum);
 
 
 console.log(campo);
